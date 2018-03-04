ff03abfa249c69e773bbe24b28c7266cf84ba5ea
berkaykrc <berkay.kirci@gmail.com>
2018-03-04 22:48:11 +0300

    modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Gonderi
+from django.utils import timezone
+
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	gonderiler = Gonderi.objects.filter(olusturma_tarihi__lte=timezone.now()).order_by('olusturma_tarihi')
+	return render(request, 'blog/post_list.html', {'gonderiler': gonderiler})
