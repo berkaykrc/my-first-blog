a288b2e23d0d1ceccf1f3eece2a2513fdcef3e87
berkaykrc <berkay.kirci@gmail.com>
2018-03-05 23:31:27 +0300

    added views to create/edit blog post inside the site and main page ordered by desc to asc.

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Gonderi(models.Model):
 	yazari = models.ForeignKey('auth.User', on_delete=models.CASCADE)#başka bir modele referans tanımlar
 	baslik = models.CharField(max_length=200)#belirli bir uzunlukta metin için tanımlanır
 	yazi = models.TextField()#uzunluğu belli olmayan bir metin uzunluğu için tanımlanır
 	olusturma_tarihi = models.DateTimeField(blank=True, null=True)#gün ay yıl bilgisi için tanımlandı
-	
+
 	def yayinla(self):
 		self.olusturma_tarihi = timezone.now()
 		self.save()
-	
+
 	def __str__(self):
 		return self.baslik
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Gonderi
 from django.utils import timezone
+from .forms import PostForm
 
 
 def post_list(request):
-	gonderiler = Gonderi.objects.filter(olusturma_tarihi__lte=timezone.now()).order_by('olusturma_tarihi')
+	gonderiler = Gonderi.objects.filter(olusturma_tarihi__lte=timezone.now()).order_by('-olusturma_tarihi')
 	return render(request, 'blog/post_list.html', {'gonderiler': gonderiler})
 def post_detail(request, pk):
 	post = get_object_or_404(Gonderi, pk=pk)
-	return render(request, 'blog/post_details.html', {'gonderi': post})
+	return render(request, 'blog/post_detail.html', {'gonderi': post})
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.yazari = request.user
+			post.olusturma_tarihi = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form' : form})
+def post_edit(request, pk):
+	post = get_object_or_404(Gonderi, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.yazari = request.user
+			post.olusturma_tarihi = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form' : form})
