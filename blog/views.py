0c728498e19eb3727563b1ab3aad23e0ed7c05d2
berkaykrc <root@Berkay.localdomain>
2018-03-05 21:10:57 +0300

    added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Gonderi
 from django.utils import timezone
 
@@ -6,3 +6,6 @@ from django.utils import timezone
 def post_list(request):
 	gonderiler = Gonderi.objects.filter(olusturma_tarihi__lte=timezone.now()).order_by('olusturma_tarihi')
 	return render(request, 'blog/post_list.html', {'gonderiler': gonderiler})
+def post_detail(request, pk):
+	post = get_object_or_404(Gonderi, pk=pk)
+	return render(request, 'blog/post_details.html', {'gonderi': post})
