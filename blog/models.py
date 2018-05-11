de99d0f20dece4805d96ead229e3ff72a37832b9
berkaykrc <berkay.kirci@gmail.com>
2018-05-11 19:48:13 +0300

    deleted turkkish comment blocks

--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,15 @@ from django.utils import timezone
 
 
 class Gonderi(models.Model):
-	yazari = models.ForeignKey('auth.User', on_delete=models.CASCADE)#başka bir modele referans tanımlar
-	baslik = models.CharField(max_length=200)#belirli bir uzunlukta metin için tanımlanır
-	yazi = models.TextField()#uzunluğu belli olmayan bir metin uzunluğu için tanımlanır
-	olusturma_tarihi = models.DateTimeField(blank=True, null=True)#gün ay yıl bilgisi için tanımlandı
+    yazari = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    baslik = models.CharField(max_length=200)
+    yazi = models.TextField()
+    olusturma_tarihi = models.DateTimeField(
+        blank=True, null=True)
 
-	def yayinla(self):
-		self.olusturma_tarihi = timezone.now()
-		self.save()
+    def yayinla(self):
+        self.olusturma_tarihi = timezone.now()
+        self.save()
 
-	def __str__(self):
-		return self.baslik
+    def __str__(self):
+        return self.baslik
