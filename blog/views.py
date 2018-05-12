4b0fb7db5e9f81951160716d44a62eaf43f40a55
berkaykrc <berkay.kirci@gmail.com>
2018-05-12 16:59:40 +0300

    -added publish and delete buttons in draft list -aded categories html pages bershka and zara -

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Gonderi
+from django.db.models import Q
 from django.utils import timezone
 from .forms import PostForm
 
@@ -15,6 +16,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Gonderi, pk=pk)
     return render(request, 'blog/post_detail.html', {'gonderi': post})
 
+
 @login_required
 def post_new(request):
     if request.method == "POST":
@@ -27,6 +29,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 @login_required
 def post_edit(request, pk):
@@ -41,10 +44,36 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
 @login_required
 def post_draft_list(request):
-    posts = Gonderi.objects.filter(olusturma_tarihi__isnull=True).order_by('olusturma_tarihi')
+    posts = Gonderi.objects.filter(olusturma_tarihi__isnull=True).\
+        order_by('olusturma_tarihi')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
-#def zara(request):
-    #gonderiler=Gonderi.objects.filter(baslik).get(gonderi_baslik__startwith='Zara')
-    #return render(request, 'blog/post_list.html', {'gonderiler' : gonderiler})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Gonderi, pk=pk)
+    post.yayinla()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Gonderi, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+def zara(request):
+    zara_items = Gonderi.objects.filter(olusturma_tarihi__lte=timezone.now()).order_by('-olusturma_tarihi')\
+        .filter(Q(baslik__contains='Zara') | Q(baslik__contains='zara'))
+    zara_image = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Zara_Logo.svg/1200px-Zara_Logo.svg.png"
+    return render(request, 'blog/zara.html', {'zara_items': zara_items})
+
+
+def bershka(request):
+    bershka_items = Gonderi.objects.filter(olusturma_tarihi__lte=timezone.now()).order_by('-olusturma_tarihi')\
+        .filter(Q(baslik__contains='Bershka') | Q(baslik__contains='bershka'))
+    bershka_image = "http://logo-logos.com/wp-content/uploads/2016/10/Bershka_logo.png"
+    return render(request, 'blog/bershka.html', {'bershka_items': bershka_items})
