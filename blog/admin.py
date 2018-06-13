a4ca24ec7a2ac49937f8d91859cad305466eaa44
berkaykrc <berkay.kirci@gmail.com>
2018-06-14 02:14:10 +0300

    adding category part in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Gonderi
+from .models import Gonderi, Comment, Categories
+
+
+class CategoryAdmin(admin.ModelAdmin):
+   list_display = ('name', 'slug')
+   prepopulated_fields = {'slug': ('name',)}
+
 
 admin.site.register(Gonderi)
+admin.site.register(Comment)
+admin.site.register(Categories, CategoryAdmin)
